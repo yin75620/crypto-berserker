4fa1f7038f9b082584ddf601a05c0c834bcd3750
yin75620 <yin75620@hotmail.com>
2019-08-16 21:35:45 +0800

    調整自己測試的程式碼

--- quicktest/main.go
+++ quicktest/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 
 	"github.com/yin75620/crypto-berserker/ftx"
 )
@@ -21,7 +24,10 @@ type LoginRequestDetail struct {
 }
 
 func main() {
+	httptest()
+}
 
+func codeText() {
 	res, _ := ftx.GetParamHmacSHA256HexSign("Y2QTHI23f23f23jfjas23f23To0RfUwX3H42fvN-", "1557246346499"+ftx.WEBSOCKET_LOGIN_KEY_WORD)
 	log.Println(res)
 	loginRequest := LoginRequest{
@@ -38,4 +44,31 @@ func main() {
 		fmt.Println("error:", err)
 	}
 	log.Println(string(loginJson))
+}
+
+func httptest() {
+
+	req, err := http.NewRequest("GET", "www.google.com", bytes.NewBuffer([]byte("body")))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	//addHeader(&req.Header, method, apiName, body)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer resp.Body.Close()
+	sitemap, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+		fmt.Printf("%s", err)
+		return
+	}
+
 }
