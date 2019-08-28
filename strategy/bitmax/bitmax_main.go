2212effa9d627f302f5d752ac10526d8f2d2f474
yin75620 <yin75620@hotmail.com>
2019-08-28 22:18:49 +0800

    增加另一個交易所

--- strategy/bitmax/bitmax_main.go
+++ strategy/bitmax/bitmax_main.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 
 	"github.com/yin75620/crypto-berserker/bitmax"
+	"github.com/yin75620/crypto-berserker/ftx"
+	"github.com/yin75620/crypto-berserker/setting"
 	"github.com/yin75620/crypto-berserker/strategy/arbitrage/Triangular"
 )
 
@@ -11,10 +13,24 @@ var mBitmax = bitmax.NewBitmax(http.DefaultClient)
 
 var m_tri = Triangular.NewTriangular(mBitmax)
 
+var mFtx = ftx.NewFtx(http.DefaultClient,
+	ftx.FtxInit{
+		setting.FTX_KEY,
+		setting.FTX_API_SECRET_KEY,
+		setting.FTX_SUBACCOUNT})
+var mTriFtx = Triangular.NewTriangular(mFtx)
+
 func main() {
+
 	m_tri.SetDealCoin([][]string{
 		[]string{"FTT", "USDT"},
 		[]string{"FTT", "BTC", "USDT"},
 	})
 	m_tri.Start()
+
+	/*mTriFtx.SetDealCoin([][]string{
+		[]string{"FTT", "USD"},
+		[]string{"FTT", "BTC", "USD"},
+	})
+	mTriFtx.Start()*/
 }
