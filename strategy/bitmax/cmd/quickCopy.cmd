40dfdaf263c3c60b6f279bb2e113ef79c9bc7c16
yin75620 <yin75620@hotmail.com>
2019-08-30 07:32:44 +0800

    add cmd for maintenance

---
+++ strategy/bitmax/cmd/quickCopy.cmd
@@ -0,0 +1,7 @@
+COPY source/bitmax_main.exe BTC/bitmax_main.exe
+COPY source/bitmax_main.exe BTMX/bitmax_main.exe
+COPY source/bitmax_main.exe BTMXP/bitmax_main.exe
+COPY source/bitmax_main.exe ETH/bitmax_main.exe
+COPY source/bitmax_main.exe FTT/bitmax_main.exe
+COPY source/bitmax_main.exe LAMB/bitmax_main.exe
+COPY source/bitmax_main.exe MITX/bitmax_main.exe
---
+++ strategy/bitmax/cmd/restart.cmd
@@ -0,0 +1,36 @@
+taskkill /IM bitmax_main.exe
+
+cd ./BTC
+start bitmax_main.exe
+cd..
+timeout /t 1
+
+cd ./BTMX
+start bitmax_main.exe
+cd..
+timeout /t 1
+
+cd ./BTMXP
+start bitmax_main.exe
+cd..
+timeout /t 1
+
+cd ./ETH
+start bitmax_main.exe
+cd..
+timeout /t 1
+
+cd ./FTT
+start bitmax_main.exe
+cd..
+timeout /t 1
+
+cd ./LAMB
+start bitmax_main.exe
+cd..
+timeout /t 1
+
+cd ./MITX
+start bitmax_main.exe
+cd..
+timeout /t 1
