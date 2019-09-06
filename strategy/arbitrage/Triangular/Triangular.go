fd5fe6cfc426d92e883b4da225eeec28709c367e
yin75620 <yin75620@hotmail.com>
2019-09-07 07:04:11 +0800

    修正 Exception 發生的捕捉功能

--- strategy/arbitrage/Triangular/Triangular.go
+++ strategy/arbitrage/Triangular/Triangular.go
@@ -308,14 +308,17 @@ var MAX_FAIL_COUNT = 3
 func (tri *Triangular) stratStrategy() int {
 
 	defer func() {
-		if err := recover(); err != nil {
-			fmt.Println(err) // 這已經是頂層的 UI 介面了，想以自己的方式呈現錯誤
+		err := recover()
+		if err == nil {
+			return
 		}
+		log.Println(err) // 這已經是頂層的 UI 介面了，想以自己的方式呈現錯誤
 		mFailCount = mFailCount + 1
 		if mFailCount < MAX_FAIL_COUNT {
 			//再重來一次
 			tri.stratStrategy()
 		}
+		log.Println(mFailCount)
 		// 失敗次數太多，直接結束
 	}()
 
