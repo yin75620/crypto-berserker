6f7b23025da20886eff3c7e49524f0e25a5eb8f5
yin75620 <yin75620@hotmail.com>
2019-10-17 13:32:38 +0800

    增加詢價測試

--- strategy/arbitrage/Triangular/Triangular_test.go
+++ strategy/arbitrage/Triangular/Triangular_test.go
@@ -1,6 +1,8 @@
 package Triangular
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"testing"
 
@@ -14,11 +16,23 @@ var mMai = maicoin.NewMaicoin(http.DefaultClient)
 var mFtx = ftx.NewFtx(http.DefaultClient, ftx.FtxInit{
 	setting.FTX_KEY,
 	setting.FTX_API_SECRET_KEY,
-	"Saber"})
+	"apicaller"})
 var mTri *Triangular = NewTriangular(mFtx)
 
 func TestExecuteOrder(t *testing.T) {
 	dealFlow := mTri.NewDealFlow("FTT", []string{"USD"})
 	mTri.Init.RangePremium = 0.02
 	mTri.executeOrder(dealFlow, exc.Ask, 1.8546)
+}
+
+func TestDealFlow(t *testing.T) {
+	dealFlow := mTri.NewDealFlow("FTT", []string{"BTC", "USD"})
+	laName := dealFlow.getName()
+
+	laPrice := dealFlow.getFinalPair(exc.Ask).Price
+
+	laVolume := dealFlow.getFinalPair(exc.Ask).Volume
+	laValue := laPrice * laVolume
+
+	log.Println(fmt.Sprintf("resAsk:%f, laValue:%f, AskCoin:%s", laPrice, laValue, laName))
 }
