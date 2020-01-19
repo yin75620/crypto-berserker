0447220bb183a72fca61528db13ca811e56d5d79
yin75620 <yin75620@hotmail.com>
2020-01-19 23:18:24 +0800

    修正 float 的轉換

--- object_tool/object_tool.go
+++ object_tool/object_tool.go
@@ -14,7 +14,7 @@ func ToString(x interface{}) string {
 	} else if i, ok := x.(int); ok {
 		value = fmt.Sprintf("%d", i)
 	} else if i, ok := x.(float64); ok {
-		value = fmt.Sprintf("%.f", i)
+		value = fmt.Sprintf("%g", i)
 	} else {
 		log.Fatal("ToString undefine type:", x)
 	}
