4f7246e9414041f4221429ab557abec1e157d0a9
JeffYin <yin75620@hotmail.com>
2020-04-03 10:05:09 +0800

    fix UnrealizedPnl json type

--- exchange-list/ftx/Ftx_structs.go
+++ exchange-list/ftx/Ftx_structs.go
@@ -50,6 +50,6 @@ type Account struct {
 		ShortOrderSize               float64 `json:"shortOrderSize"`
 		Side                         string  `json:"side"`
 		Size                         float64 `json:"size"`
-		UnrealizedPnl                int     `json:"unrealizedPnl"`
+		UnrealizedPnl                float64 `json:"unrealizedPnl"`
 	}
 }
