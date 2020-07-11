33c2b37bc23c0a2a86cd821809d1b84cc331c61c
JeffYin <yin75620@hotmail.com>
2020-07-11 23:13:39 +0800

    quieck test key in

--- quicktest/main.go
+++ quicktest/main.go
@@ -29,7 +29,14 @@ type LoginRequestDetail struct {
 }
 
 func main() {
-	changelog()
+	keyIn()
+}
+
+func keyIn() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Println(text)
 }
 
 func changelog() {
