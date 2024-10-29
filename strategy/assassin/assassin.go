002e0a722b5e9f256ec21fdaeb2abfef8cb209d1
Jeff Yin <yin75620@gmail.com>
2024-10-29 21:07:25 +0800

    fix:split db save function

--- strategy/assassin/assassin.go
+++ strategy/assassin/assassin.go
@@ -4,29 +4,46 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/yin75620/crypto-berserker/exchange"
 	"github.com/yin75620/crypto-berserker/exchange-list/common"
+	"github.com/yin75620/crypto-berserker/jtime"
 	"github.com/yin75620/crypto-berserker/strategy/assassin/db"
+	"gorm.io/gorm"
 )
+
+var BINANCE_BTC_START_TIME = jtime.UnixToTime(1567965420000)
 
 func main() {
 	collector := common.GetCollector("")
-
-	// startTime, _ := time.Parse(time.RFC3339, "2024-01-01T00:00:00Z")
-	// endTime, _ := time.Parse(time.RFC3339, "2024-01-02T00:00:00Z")
-	startTime := time.Time{}
-	endTime := time.Time{}
-
-	klines, err := collector.GetKlines("BTCUSDT", "1m", startTime, endTime, 3)
-	if err != nil {
-		fmt.Println("Error fetching Klines:", err)
-		return
-	}
 
 	// 連接 MySQL 資料庫
 	dbmanager := db.NewDBManager()
 	gormDB, err := dbmanager.OpenGormDB()
 	if err != nil {
 		fmt.Println(err)
+	}
+
+	// startTime, _ := time.Parse(time.RFC3339, "1980-01-01T00:00:00Z")
+	// endTime, _ := time.Parse(time.RFC3339, "2024-01-02T00:00:00Z")
+	barCount := 1000
+
+	// startTime := BINANCE_BTC_START_TIME
+	// endTime := startTime.Add(time.Duration(barCount) * time.Second)
+	now := time.Now()
+
+	for ti := BINANCE_BTC_START_TIME; ti.Compare(now) < 1; ti = ti.Add(time.Duration(barCount) * time.Second) {
+		startTime := ti
+		endTime := startTime.Add(time.Duration(barCount) * time.Second)
+		save_bar_into_db(gormDB, collector, startTime, endTime, barCount)
+	}
+}
+
+func save_bar_into_db(gormDB *gorm.DB, collector exchange.Collector, startTime, endTime time.Time, barCount int) {
+
+	klines, err := collector.GetKlines("BTCUSDT", "1m", startTime, endTime, barCount)
+	if err != nil {
+		fmt.Println("Error fetching Klines:", err)
+		return
 	}
 
 	// 顯示取得的 K 線資料
--- strategy/assassin/assassin_test.go
+++ strategy/assassin/assassin_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/yin75620/crypto-berserker/exchange-list/common"
+	"github.com/yin75620/crypto-berserker/jtime"
+	"github.com/yin75620/crypto-berserker/strategy/assassin/db"
+)
 
 func TestMain(t *testing.T) {
 	main()
+}
+
+func TestFree(t *testing.T) {
+	fmt.Println(jtime.UnixToTime(1567965420000))
+}
+
+func TestEarly(t *testing.T) {
+	collector := common.GetCollector("")
+
+	// 連接 MySQL 資料庫
+	dbmanager := db.NewDBManager()
+	gormDB, err := dbmanager.OpenGormDB()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	save_bar_into_db(gormDB, collector, jtime.UnixToTime(1566965420000), jtime.UnixToTime(1577965420000), 1)
 }
